d5372cb9634a2de2c439bdc84658918f251dd720
David <Davidteri91@gmail.com>
2016-04-22 16:41:53 +0200

    sony aosp blobs: released 2016-04-22
    From: http://developer.sonymobile.com/knowledge-base/open-source/open-devices/latest-updates/

--- kitakami-suzuran/suzuran-partial.mk
+++ kitakami-suzuran/suzuran-partial.mk
@@ -14,4 +14,14 @@
 
 PRODUCT_PACKAGES += \
 	fw_bcmdhd \
-	fw_bcmdhd_apsta
+	fw_bcmdhd_apsta \
+	keymaster_b00 \
+	keymaster_b01 \
+	keymaster_b02 \
+	keymaster_b03 \
+	keymaster_mdt \
+	tzfingerprint_b00 \
+	tzfingerprint_b01 \
+	tzfingerprint_b02 \
+	tzfingerprint_b03 \
+	tzfingerprint_mdt
